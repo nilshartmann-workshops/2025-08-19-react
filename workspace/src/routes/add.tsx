14de1bcf55819801ca17c0ad0c13742cf799d6db
Nils Hartmann <nils@nilshartmann.net>
2025-08-12 15:57:36 +0200

    10: Form Basics

--- workspace/src/routes/add.tsx
+++ workspace/src/routes/add.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import PlantForm from "../components/PlantForm.tsx";
+
 export const Route = createFileRoute("/add")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/add"!</div>;
+  return <PlantForm />;
 }

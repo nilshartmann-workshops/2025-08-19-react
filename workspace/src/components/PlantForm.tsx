66e2ff770b845ea61fe644b5cf3cc54557e919cb
Nils Hartmann <nils@nilshartmann.net>
2025-08-20 15:25:56 +0200

    React Hook Form Controller und FormContext

--- workspace/src/components/PlantForm.tsx
+++ workspace/src/components/PlantForm.tsx
@@ -1,19 +1,27 @@
 import { z } from "zod/v4";
-import { useForm, useWatch } from "react-hook-form";
+import {
+  Controller,
+  FormProvider,
+  useForm,
+  useFormContext,
+  useWatch,
+} from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { getPlantByIdOpts } from "../queries.ts";
 import { Plant } from "../types.ts";
 import { useState } from "react";
+import IntervalSelector from "./IntervalSelector.tsx";
 
 const PlantFormState = z.object({
   name: z.string().nonempty("Bitte Pflanzenamen eingeben"),
   location: z.string().nonempty(),
-  lastWatered: z.iso.date().optional()
+  lastWatered: z.iso.date().optional(),
+  wateringInterval: z.number(),
   //                .transform(
   //   v => v === "" ? undefined : v
   // )
-});
+})
 
 // const XPlantFormState = PlantFormState.extend({
 //   lastWatered: z.iso.date().optional()
@@ -22,12 +30,13 @@ const PlantFormState = z.object({
 //   )
 // })
 
-
 type PlantFormState = z.infer<typeof PlantFormState>;
 
-
-export default function PlantForm({existingPlant}:{existingPlant?: Plant}) {
-
+export default function PlantForm({
+  existingPlant,
+}: {
+  existingPlant?: Plant;
+}) {
   // const response = useQuery(
   //   {...getPlantByIdOpts("1"),
   //   select
@@ -36,93 +45,115 @@ export default function PlantForm({existingPlant}:{existingPlant?: Plant}) {
 
   const form = useForm<PlantFormState>({
     resolver: zodResolver(PlantFormState),
-    defaultValues: existingPlant ? existingPlant : {
-
-    },
+    defaultValues: existingPlant ? {
+    } : {},
     mode: "onBlur",
     // reValidateMode: "onBlur"
   });
 
   const handleFormSubmit = (data: PlantFormState) => {
     console.log("data", data);
-  }
+  };
 
   const handleError = (err: any) => {
     console.log("FORM ERRORS", err);
-  }
+  };
 
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
 
-const plantName  =""
+  const plantName = "";
   // const [plantName] = form.watch(["name"])
 
-  return <form
-    // onReset={() => form.reset()}
-    onSubmit={form.handleSubmit(handleFormSubmit,handleError)}>
-    <button type={"button"} onClick={() => setCounter(counter +1 )}>
-      Increase{counter}
-    </button>
-    <p>Pflanzenname: {plantName}</p>
-    {/*<PlantName watch={form.watch} />*/}
-    <PlantNameMitControl control={form.control} />
-    <div className={"FormControl"}>
-      <label>
-        Name
-      </label>
-      <input
-        {...form.register("name")}
-      />
-      <p className={"error-message"}> {form.formState.errors.name?.message}</p>
-    </div>
-    <div className={"FormControl"}>
-      <label>
-        Standort
-      </label>
-      <select {...form.register("location")}  >
-        <option></option>
-        <option>Wohnzimmer</option>
-        <option>Schlafzimmer</option>
-        <option>Küche</option>
-      </select>
-    </div>
+  return (
+    <FormProvider {...form}>
+      <form
+        // onReset={() => form.reset()}
+        onSubmit={form.handleSubmit(handleFormSubmit, handleError)}
+      >
+        <button type={"button"} onClick={() => setCounter(counter + 1)}>
+          Increase{counter}
+        </button>
+        <p>Pflanzenname: {plantName}</p>
+        {/*<PlantName watch={form.watch} />*/}
+        <PlantNameMitControl control={form.control} />
+        <div className={"FormControl"}>
+          <label>Name</label>
+          <input {...form.register("name")} />
+          <p className={"error-message"}>
+            {" "}
+            {form.formState.errors.name?.message}
+          </p>
+        </div>
+
+        <Controller
+          control={form.control}
+          name={"wateringInterval"}
+          render={(field) => {
+            return (
+              <IntervalSelector
+                interval={field.field.value}
+                onIntervalChange={field.field.onChange}
+              />
+            );
+          }}
+        />
+
+        <LastWateredInput />
+
+        <div className={"FormControl"}>
+          <label>Standort</label>
+          <select {...form.register("location")}>
+            <option></option>
+            <option>Wohnzimmer</option>
+            <option>Schlafzimmer</option>
+            <option>Küche</option>
+          </select>
+        </div>
+
+        <div className={"FormButtons"}>
+          {/*<button type={"reset"}>Reset</button>*/}
+
+          <button type={"button"} onClick={() => form.reset()}>
+            Reset
+          </button>
+          <button className={"primary"}>Speichern</button>
+        </div>
+        <Label />
+      </form>
+    </FormProvider>
+  );
+}
+
+function LastWateredInput() {
+  const form = useFormContext<PlantFormState>();
+
+  return (
     <div className={"FormControl"}>
       <label>Zuletzt gewässert</label>
-      <input type={"date"}
+      <input
+        type={"date"}
         {...form.register("lastWatered", {
-          setValueAs: value => value === "" ? undefined : value
+          setValueAs: (value) => (value === "" ? undefined : value),
         })}
       />
     </div>
-
-    <div className={"FormButtons"}>
-      {/*<button type={"reset"}>Reset</button>*/}
-
-      <button type={"button"} onClick={() => form.reset()}>Reset</button>
-      <button className={"primary"}>Speichern</button>
-    </div>
-    <Label />
-
-  </form>
+  );
 }
 
 function Label() {
-  return <div>Huhu</div>
+  return <div>Huhu</div>;
 }
 
-function PlantName({watch}: {watch: any}) {
-  const [plantName] = watch(["name"])
-  return <div>
-    Plantname: {plantName}
-  </div>
+function PlantName({ watch }: { watch: any }) {
+  const [plantName] = watch(["name"]);
+  return <div>Plantname: {plantName}</div>;
 }
 
-function PlantNameMitControl({control}: {control: any}) {
+function PlantNameMitControl({ control }: { control: any }) {
   const plantName = useWatch({
     control,
-    name: "name"
-  })
+    name: "name",
+  });
   // const [plantName] = watch(["name"])
-  return <div>
-    Plantname: {plantName}
-  </div>
+  return <div>Plantname: {plantName}</div>;
 }
